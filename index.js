b692baa7fdd935a0ac6026b0ef4bb0c2cbd4e201
clarareina <reinaromeroclara@gmail.com>
2025-03-06 21:58:05 +0100

    cambios index.html

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express"); //carga el modulo que hemos intalado en una 
 
 const app = express();
 
-const PORT = 16078;
+const PORT = process.env.PORT || 16078;
 
 app.use("/", express.static("./public")); 
 
