5b62fa3af03572f904b999f4c52149bfb8b96276
clarareina <reinaromeroclara@gmail.com>
2025-03-11 18:22:45 +0100

    update

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 console.log("Init script.");
 
-const cool = require("cool-ascii-faces");
-
 const express = require("express"); //carga el modulo que hemos intalado en una variable
-
 const app = express();
-
 const PORT = process.env.PORT || 16078;
+const BASE_API = "/api/v1";
 
 app.use("/", express.static("./public")); 
 
-app.get("/hello", (request, response) => {
-    response.send("Hello from the server!")
-}
-);
+//api contactos 
+let contacts = [
+    {
+        name : "peter",
+        phone : 1234
+    },
+    {
+        name : "pablo",
+        phone : 7890
+    }
+]
 
-app.get("/cool", (request, response) => {
-    response.send(cool());
+
+app.get(BASE_API+"/contacts", (request, response) => {
+    console.log("New GET to /contacts");
+    response.send(JSON.stringify(contacts));
 });
 
 app.listen(PORT, () => {  //cuando esté escuchando, llama al metodo
