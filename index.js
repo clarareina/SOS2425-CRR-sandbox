2c352e464083bcb9b0f70f5c5ba8f3f63736b8a7
clarareina <reinaromeroclara@gmail.com>
2025-03-18 17:57:46 +0100

    update

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 16078;
 const BASE_API = "/api/v1";
 
+
 app.use("/", express.static("./public")); 
+app.use(express.json());
 
 //api contactos 
 let contacts = [
@@ -22,8 +24,36 @@ let contacts = [
 
 app.get(BASE_API+"/contacts", (request, response) => {
     console.log("New GET to /contacts");
-    response.send(JSON.stringify(contacts));
+    response.send(JSON.stringify(contacts)); //lo que le envio al cliente
 });
+
+app.post(BASE_API+"/contacts", (request, response) => {
+    console.log("New POST to /contacts");
+    let newContact = request.body;  //lo que hay dentro de los datos que me han mandado en la petición
+    contacts.push(newContact);
+    response.sendStatus(201) //lo que le envio al cliente
+}); 
+
+app.put(BASE_API + "/contacts/:phone", (request, response) => {
+        const phone = request.params.phone;
+        console.log(`New PUT to /contacts/${phone}`);
+    
+        const search = contacts.filter(x => x.phone === phone);
+        if (search.length > 0){
+            let data = request.body;
+            if (!data.name || !data.phone) {
+                response.status(400).json({message : "Faltan datos requeridos"});
+            }
+            else {
+                
+                response.status(200).json({message : "Datos actualizados"});
+            }
+        }
+        else{   
+            return response.status(404).json({error: `No se encuentran datos de ${phone}`});
+        }
+    
+    });
 
 app.listen(PORT, () => {  //cuando esté escuchando, llama al metodo
     console.log(`Server running on the port ${PORT}!`);  //como la primera es asincrona, tenemos q ponerlo así
